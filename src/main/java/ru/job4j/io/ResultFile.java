10fab2557da2679f5e9bc0f1ae0a41bf493466ee
andrezrkn <vartan.andreevich@yandex.ru>
2022-02-09 15:37:50 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+
+    public static int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                table[i - 1][j - 1] = i * j;
+            }
+        }
+        return table;
+    }
+
+    public static void main(String[] args) {
+        int size = 5;
+        int[][] table = multiple(size);
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    out.write((byte) table[i][j]);
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
