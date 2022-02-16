ce2af89a9623dc471896c706272d99c38bad4f7c
andrezrkn <vartan.andreevich@yandex.ru>
2022-02-16 23:49:10 +0300

    2. Анализ доступности сервера. [#859 #264486]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -5,54 +5,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Analizy {
-    private List<String> logic(List<String> data) {
-        List<String> result = new ArrayList<>();
-        boolean flag = false, endOfPeriod = false;
-        String period = "";
-        for (String el : data) {
-            if (!flag && (el.contains("400 ") || el.contains("500 "))) {
-                flag = true;
-                period += el.substring(4) + ";";
-            } else if (flag && (el.contains("200 ") || el.contains("300 "))) {
-                flag = false;
-                endOfPeriod = true;
-                period += el.substring(4);
-            }
-            if (endOfPeriod) {
-                endOfPeriod = false;
-                result.add(period);
-                period = "";
-            }
-        }
-        return result;
-    }
-
-    public void unavailable(String source, String target) {
-        boolean flag = false;
-        List<String> data = new ArrayList<>();
-        List<String> result;
-
-        try (BufferedReader in = new BufferedReader(
-                new FileReader(source))) {
-            for (String line = in.readLine(); line != null;
-                 line = in.readLine()) {
-                data.add(line);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        result = logic(data);
-
+    private void save(String data, String target) {
         try (PrintWriter out = new PrintWriter(
                 new BufferedOutputStream(
                         new FileOutputStream(target)
                 ))) {
-            for (String el : result) {
-                out.println(el);
+                out.println(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void unavailable(String source, String target) {
+        List<String> result = new ArrayList<>();
+
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+            boolean flag = false, endOfPeriod = false;
+            StringBuilder period = new StringBuilder();
+            for (String line = in.readLine(); line != null;
+                 line = in.readLine()) {
+                if (!flag && (line.contains("400 ") || line.contains("500 "))) {
+                    flag = true;
+                    period.append(line.substring(4)).append(";");
+                } else if (flag && (line.contains("200 ") || line.contains("300 "))) {
+                    flag = false;
+                    endOfPeriod = true;
+                    period.append(line.substring(4));
+                }
+                if (endOfPeriod) {
+                    endOfPeriod = false;
+                    save(period.toString(), target);
+                    period = new StringBuilder();
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        Analizy ob = new Analizy();
+        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ob.unavailable("unavailable.csv", "file.txt");
     }
 }
