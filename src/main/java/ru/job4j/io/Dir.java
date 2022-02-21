570a83b3f5b4c28b901ef91d24c9915deaaf11de
andrezrkn <vartan.andreevich@yandex.ru>
2022-02-21 11:45:26 +0300

    4.0. File [#252491 #267190]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class Dir {
+    public static void main(String[] args) throws IOException {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        Files.walkFileTree(file.toPath(), new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                File f = new File(file.toAbsolutePath().toString());
+                if (f.exists()) {
+                    System.out.printf(file.getFileName() + " %d", f.length());
+                    System.out.println("");
+                }
+                return super.visitFile(file, attrs);
+            }
+        });
+    }
+}
