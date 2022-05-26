27d6e228160251e119d601f1480a2c89e863b75a
andrezrkn <vartan.andreevich@yandex.ru>
2022-05-26 12:20:30 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -14,9 +14,13 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("Bye")) {
+                        if (str.contains("Exit")) {
                             server.close();
                             System.out.println("server closed");
+                        } else if (str.contains("Hello")) {
+                            System.out.println("Hello");
+                        } else if (str.contains("/?msg=")) {
+                            System.out.println(str.substring(10, str.length() - 9));
                         }
                         System.out.println(str);
                     }
