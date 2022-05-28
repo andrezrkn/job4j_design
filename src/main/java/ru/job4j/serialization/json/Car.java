18741d506c3a5f4dd51c2865632d0fcf5b547ef5
andrezrkn <vartan.andreevich@yandex.ru>
2022-05-28 12:34:08 +0300

    2. Формат JSON [#313164]

---
+++ src/main/java/ru/job4j/serialization/json/Car.java
@@ -0,0 +1,31 @@
+package ru.job4j.serialization.json;
+
+import java.util.Arrays;
+
+public class Car {
+    private final boolean operativeCondition;
+    private final int weight;
+    private final String name;
+    private final Engine engine;
+    private final String[] competitors;
+
+    public Car(boolean operativeCondition, int weight, String name,
+               Engine engine, String[] competitors) {
+        this.operativeCondition = operativeCondition;
+        this.weight = weight;
+        this.name = name;
+        this.engine = engine;
+        this.competitors = competitors;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "operativeCondition=" + operativeCondition
+                + ", weight=" + weight
+                + ", name='" + name + '\''
+                + ", engine=" + engine
+                + ", competitors='" + Arrays.toString(competitors) + '\''
+                + '}';
+    }
+}
