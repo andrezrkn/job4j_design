d46dd4696fbfe264868479d09bdbbe29ece15bfd
andrezrkn <vartan.andreevich@yandex.ru>
2024-05-05 15:36:43 +0300

    4. Утверждения с исключениями [#504886]

--- src/test/java/ru/job4j/assertj/NameLoadTest.java
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -2,7 +2,6 @@ package ru.job4j.assertj;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class NameLoadTest {
 
